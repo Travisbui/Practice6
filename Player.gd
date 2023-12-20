5c27fe545bccaa55326387a013b057f659424164
Travisbui <122054086+Travisbui@users.noreply.github.com>
2023-12-19 20:33:02 -0800

    commit5

--- Player.gd
+++ Player.gd
@@ -12,6 +12,9 @@ func _process(delta):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if velocity.length() > 1:
+	if velocity.length() > 0:
 		velocity = velocity.normalized() * Speed
 	position += velocity * delta
+
+func _on_body_entered(body):
+	hide()
